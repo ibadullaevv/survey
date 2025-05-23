9be0f7dc555085ac5f4cf0aabd9fa6d7629c6f40
Xursandbek Ibadullayev <xursandbek1014@gmail.com>
2025-05-23 10:57:39 +0500

    change urls

--- sample_survey/settings.py
+++ sample_survey/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'bh-+xvs)tw0b*vuuw7f@(z%9a)jo%^dq=^!ame@$rrh2ay2)^t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
