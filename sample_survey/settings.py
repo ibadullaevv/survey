ec50ec517fab0b6edf73c7e8a39114d3f8fbf852
xursandbek-horizon <ibadullaevkhursandbek@gmail.com>
2025-06-21 12:26:45 +0500

    bdfdbfbf

--- sample_survey/settings.py
+++ sample_survey/settings.py
@@ -25,14 +25,14 @@ SECRET_KEY = 'bh-+xvs)tw0b*vuuw7f@(z%9a)jo%^dq=^!ame@$rrh2ay2)^t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['survey.urdu.uz', 'www.survey.urdu.uz']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['survey.urdu.uz', 'www.survey.urdu.uz']
 CSRF_TRUSTED_ORIGINS = ["https://survey.urdu.uz", "https://www.survey.urdu.uz"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'jet',
+    # 'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
